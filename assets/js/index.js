93c865b5fd3a11d73a051a5139ddd3851fed9167
NerVod <benjamin.jeannerot@gmail.com>
2022-01-22 17:57:40 +0100

    proj1

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,21 +15,33 @@ const ReactDOM = require("react-dom");
 
 // ReactDOM.render(<h1 className = "header">Hello, Everyone !</h1>, document.getElementById("root"));
 
-const navbar = (
-  <nav>
-    <h1>My React static page !</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-);
+// const navbar = (
+//   <nav>
+//     <h1>My React static page !</h1>
+//     <ul>
+//       <li>Pricing</li>
+//       <li>About</li>
+//       <li>Contact</li>
+//     </ul>
+//   </nav>
+// );
 
-ReactDOM.render(navbar, document.getElementById("root"));
+// ReactDOM.render(navbar, document.getElementById("root"));
 
-let alerte = function () {
-  alert("la page est trouvée");
+const page = (
+  <div>
+      <img src="./react-logo.png" alt="logo react" width="40px"></img>
+      <h1>Fun facts about React</h1>
+          <ul>
+              <li>Was first released in 2013</li>
+              <li>Was originally created by Jordan Walke</li>
+              <li>Has well over 100K stars on GitHub</li>
+              <li>Is maintained by FaceBook</li>
+              <li>Powers thousands of enterprise apps, includin mobile apps </li>
+          </ul>
+  </div> 
+)
 
-}
-alert()
+
+
+ReactDOM.render(page, document.getElementById("root"))
--- proj1/src/App.js
+++ proj1/src/App.js
@@ -25,17 +25,63 @@ import "./App.css";
 // export default App;
 
 
-function App() {
-  return (
-    <nav>
-    <h1>My React static page !</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-)
-}
+// function App() {
+//   return (
+//     <nav>
+//     <h1>My React static page !</h1>
+//     <ul>
+//       <li>Pricing</li>
+//       <li>About</li>
+//       <li>Contact</li>
+//     </ul>
+//   </nav>
+// )
+// }
 
-export default App;
+// export default App;
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// const page = (
+//   <div>
+//       <img src="logo192.png" alt="logo react" width="40px"></img>
+//       <h1>Fun facts about React</h1>
+//           <ul>
+//               <li>Was first released in 2013</li>
+//               <li>Was originally created by Jordan Walke</li>
+//               <li>Has well over 100K stars on GitHub</li>
+//               <li>Is maintained by FaceBook</li>
+//               <li>Powers thousands of enterprise apps, includin mobile apps </li>
+//           </ul>
+//   </div> 
+// )
+
+// function lapage () {
+//   return page
+// }
+// export default lapage
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+function MyReactPage() {
+  return (
+      <div>
+        <header>
+          <nav>
+              {/* <img src="logo512.png" alt="logo react" width="40px"></img> */}
+              <img src={logo} className="App-logo" alt="logo" width="100px"/>
+          </nav>
+        </header>
+        <h1>Pourquoi j'apprends React ?</h1>
+        <p>Dans cette page je vais lister pourquoi je suis hypé par React alors qu'Angular est au programme de l'Ifocop</p>
+        <ol>
+          <li>C'est LA techno demandée sur le marché !</li>    
+          <li>Ca fait classe de dire que tu code avec React</li>    
+          <li>Même Kamil a sorti une app en React, alors pourquoi pas moi ?</li>    
+        </ol>
+        <footer>
+          <small>© 2022  NerVod development, All rights reserved </small>
+          </footer>
+      </div>
+      
+  )
+}
+// ReactDOM.render(<MyReactPage /> , document.getElementById("root"));
+export default MyReactPage
